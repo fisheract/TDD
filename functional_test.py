3fa6c4d7820a16f3fd003ef73182f4d0b880968e
Olzhas Bekbatyr <37829206+fisheract@users.noreply.github.com>
2020-12-10 16:02:01 +0600

    Inserted first Functional testing with using unittest

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,22 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        #установка
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        #демонтаж
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrive_it_later(self):
+        self.browser.get('http://localhost:8000')
+        '''Check if header is give "To-Do" title'''
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('End test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
