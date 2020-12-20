4c6ea9b9b9056ab401bdd68f1ae915827d6d7bf8
Olzhas Bekbatyr <37829206+fisheract@users.noreply.github.com>
2020-12-20 16:30:09 +0600

    add some check as if text is present in table

--- functional_test.py
+++ functional_test.py
@@ -14,6 +14,12 @@ class NewVisitorTest(unittest.TestCase):
         #демонтаж
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        #conformation that strings are in lists
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrive_it_later(self):
         self.browser.get('http://localhost:8000')
         '''Check if header is give "To-Do" title'''
@@ -29,6 +35,8 @@ class NewVisitorTest(unittest.TestCase):
         #after pushing Enter page reload and have one to-do item "Buys ome feather"
         input_box.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1. Buy some feather')
+
         # Insert second element
         input_box = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(input_box.get_attribute('placeholder'), 'Enter a to-do item')
@@ -37,12 +45,12 @@ class NewVisitorTest(unittest.TestCase):
         # after pushing Enter page reload and have one to-do item "Buys ome feather"
         input_box.send_keys(Keys.ENTER)
         time.sleep(1)
-
+        self.check_for_row_in_list_table('1. Make pillow from feather')
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn('1. Buy some feather', [row.text for row in rows])
-        self.assertIn('2. Make pillow from feather', [row.text for row in rows])
+        self.assertIn('2. Make pillow from feather:wq', [row.text for row in rows])
         # testing field still asking to add another element
         # make pillow from feather
         self.fail('End test!')
